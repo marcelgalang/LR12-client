0de97dc93d68d0264696af71cf57221966265cc9
github username <github email address>
2017-06-15 11:56:08 -0400

    Add react-toolbox, start store/redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import theme from './toolbox/theme';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { store } from './redux/store'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById('root')
+);
