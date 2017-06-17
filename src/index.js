dd9410a42c99cc7e437e71b5b53092e5b2e427cc
github username <github email address>
2017-06-17 11:42:01 -0400

    remove redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 import theme from './toolbox/theme';
 import App from './App';
 import './index.css';
-import { store } from './redux/store'
+
 
 ReactDOM.render(
-  <Provider store={store}>
     <ThemeProvider theme={theme}>
       <App />
-    </ThemeProvider>
-  </Provider>,
+    </ThemeProvider>,
   document.getElementById('root')
 );
