16fd9a0bf48f7a5c6412b916e886e298561d7ac5
github username <github email address>
2017-08-05 09:22:46 -0400

    Add directories and server files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 import theme from './toolbox/theme';
 import App from './App';
 import './index.css';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { createStore, applyMiddleware } from 'redux'
+import reducer from './reducers'
+import thunk from 'redux-thunk'
+import { getAllProducts } from './actions'
+import {createLogger} from 'redux-logger'
 
 
 ReactDOM.render(
